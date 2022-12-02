64172006a4c03c219ad7489b60471d49eb2dc8cc
shibuya-n <a.duong2006@gmail.com>
2022-12-02 10:06:12 -0800

    victor

---
+++ src/TwoDee.java
@@ -0,0 +1,36 @@
+//Create a program that populates a 3x3 2D array with random integers ranging from 1-10.
+//Once you can generate random 2D arrays, write a method that checks if any rows contain all of the same numbers. If so, your method should print that row.
+import java.util.Arrays;
+
+public class TwoDee {
+    public static int[][] randNumbers = {
+            {0, 0, 0},
+            {0, 0, 0},
+    };
+    public static void main(String[] args) {
+        arrayFill();
+        arrayCheck();
+    }
+
+    public static void arrayFill() {
+        for (int i = 0; i < randNumbers.length; i++) {
+            for (int j = 0; j < randNumbers[i].length; j++) {
+                randNumbers[i][j] =  ((int)Math.floor(Math.random() * 10) + 1);
+
+            }
+            System.out.println(Arrays.toString(randNumbers[i]));
+        }
+        System.out.println("");
+    }
+    public static void arrayCheck() {
+        for (int i = 0; i < randNumbers.length; i++) {
+            if ((randNumbers[i][0] == (randNumbers[i][1])) && (randNumbers[i][0] == (randNumbers[i][2]))) {
+                System.out.println(Arrays.toString(randNumbers[i]));
+            }
+
+            }
+        System.out.println("No matches!");
+            }
+        }
+
+
